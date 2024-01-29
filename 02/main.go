cfe303cafe3a8fa6bdae968fcf6b32aabceabc98
Marcus Silva <mvsilva1997@gmail.com>
2024-01-29 12:38:14 -0300

    ex 02

---
+++ 02/main.go
@@ -0,0 +1,22 @@
+// Maior entre três números:
+// Escreva uma função que receba três números como entrada e retorne o maior deles.
+package main
+
+import "fmt"
+
+func maiorEntreTres(a, b, c int) interface{} {
+	if a > b && a > c {
+		return a
+	} else if b > a && b > c {
+		return b
+	} else if c > a && c > b {
+		return c
+	} else {
+		return "Nenhum número é maior"
+	}
+}
+
+func main() {
+	resultado := maiorEntreTres(8, 2, 5)
+	fmt.Println(resultado)
+}
